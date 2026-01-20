436abaaa88ce569daf0cbdc72d2835f22bc8c28c
BelalZuhair <109172554+BelalZuhair@users.noreply.github.com>
2026-01-20 04:22:30 -0800

    Add version information for whatsapp-web.js

---
+++ whatsapp-web.js/version.js
@@ -0,0 +1,15 @@
+{
+    "engine":  {
+                   "version":  "26.1.20",
+                   "library":  "whatsapp-web.js",
+                   "minServerJsVersion":  "26.1.20",
+                   "sha256":  "04FF8AC9F4875C798CCF605362C0E53B8F11931B27BA23B411D1F8F9F93DE7A8",
+                   "size":  14540551
+               },
+    "serverJs":  {
+                     "version":  "26.1.20",
+                     "sha256":  "7831298235E14569A1DC9124FB6A5F81E8359ED87BFB35D72BD7A9CF02F0CF5D",
+                     "size":  6744
+                 },
+    "created":  "2026-01-20 08:55 AM"
+}
